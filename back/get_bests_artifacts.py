7512c52e22e8434d273206fd3040ceee9b748364
tbellicha <tanguy.bellicha@epitech.eu>
2024-01-24 03:38:52 +0100

    [FIX] Reorganize how the response of the best attributes artifacts is

--- back/get_bests_artifacts.py
+++ back/get_bests_artifacts.py
@@ -13,37 +13,38 @@ get_bests_artifacts_blueprint = Blueprint(
 def artifact_best_attributes(filtered_artifacts, req_sub_stats, size):
     response = [
         [
-            [{"artifact": {}, "value": 0} for i in range(size)]
-            for k in range(len(req_sub_stats))
+            [{"artifact": {}, "value": 0} for m in range(len(req_sub_stats))]
+            for k in range(size)
         ]
-        for m in range(len(Artifact.ATTRIBUTE_STRINGS.items()))
+        for i, _ in enumerate(Artifact.ATTRIBUTE_STRINGS.items())
     ]
+    # List to not duplicate artifacts in the response
+    artifacts_list = []
     # Get the best sub_stats of artifacts of each attribute
-    for artifact in filtered_artifacts:
-        for i, (attribute_id, attribute_string) in enumerate(
-            Artifact.ATTRIBUTE_STRINGS.items()
-        ):
-            # if the current artifact is of the current attribute
-            if artifact.attribute == attribute_id:
-                for k, req_substat in enumerate(req_sub_stats):
-                    # if the current artifact has the current substat
-                    for m, substat_effect in enumerate(artifact.sec_effects):
-                        if substat_effect[0] == req_substat:
-                            # if the current artifact has a better value for the current substat than the worst artifact in the list
-                            if substat_effect[1] > response[i][k][size - 1]["value"]:
-                                # if the current artifact is not already in the list
-                                if artifact not in response[i][k]:
-                                    response[i][k].append(
-                                        {
-                                            "artifact": artifact.toJSON(),
-                                            "value": substat_effect[1],
-                                        }
-                                    )
-                                    response[i][k].sort(
-                                        key=lambda x: x["value"], reverse=True
-                                    )
-                                    response[i][k] = response[i][k][:size]
-                                    break
+    for i, attribute in enumerate(Artifact.ATTRIBUTE_STRINGS.items()):
+        artifacts_list.clear()
+        for k in range(size):
+            for m, req_substat in enumerate(req_sub_stats):
+                curr_best_value = response[i][k][m]["value"]
+                curr_best_artifact = {}
+                for artifact in filtered_artifacts:
+                    if artifact.attribute != attribute[0]:
+                        continue
+                    # Get {size} bests artifact for each {req_sub_stats} in each of the {Artifact.ATTRIBUTE_STRINGS.items()}
+                    for o, artif_substat in enumerate(artifact.sec_effects):
+                        if artif_substat[0] == req_substat:
+                            if (
+                                artif_substat[1] >= curr_best_value
+                                and artifact not in artifacts_list
+                            ):
+                                response[i][k][m]["artifact"] = artifact.toJSON()
+                                if curr_best_artifact in artifacts_list:
+                                    artifacts_list.remove(curr_best_artifact)
+                                curr_best_value = artif_substat[1]
+                                curr_best_artifact = artifact
+                                artifacts_list.append(artifact)
+                                break
+                response[i][k][m]["value"] = curr_best_value
     return jsonify(response)
 
 
@@ -62,9 +63,9 @@ def get_bests_artifacts():
     sub_stats_param = request.args.get("sub_stats", "", type=str)
     type = request.args.get("type", 0, type=int)
     try:
-        sub_stats = ast.literal_eval(sub_stats_param)
-        if not isinstance(sub_stats, list) or not all(
-            isinstance(stat, int) for stat in sub_stats
+        req_sub_stats = ast.literal_eval(sub_stats_param)
+        if not isinstance(req_sub_stats, list) or not all(
+            isinstance(stat, int) for stat in req_sub_stats
         ):
             raise ValueError("Invalid value for 'sub_stats'")
     except (ValueError, SyntaxError):
@@ -78,12 +79,12 @@ def get_bests_artifacts():
         ):
             continue
         for i, curr_substat in enumerate(artifact.sec_effects):
-            if curr_substat[0] in sub_stats:
+            if curr_substat[0] in req_sub_stats:
                 filtered_artifacts.append(artifact)
                 break
     if type == 1:
-        return artifact_best_attributes(filtered_artifacts, sub_stats, size)
-    return artifact_best_unit_styles(filtered_artifacts, sub_stats, size)
+        return artifact_best_attributes(filtered_artifacts, req_sub_stats, size)
+    return artifact_best_unit_styles(filtered_artifacts, req_sub_stats, size)
 
     # for i, artifact in enumerate(filtered_artifacts):
     #     if artifact.attribute == 1:
