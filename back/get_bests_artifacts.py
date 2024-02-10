fba7e1113218ff2a7a486c4b731ef557bc6e9437
tbellicha <tanguy.bellicha@epitech.eu>
2024-02-10 08:59:49 +0100

    [ADD] Get bests styles artifs

--- back/get_bests_artifacts.py
+++ back/get_bests_artifacts.py
@@ -48,9 +48,42 @@ def artifact_best_attributes(filtered_artifacts, req_sub_stats, size):
     return jsonify(response)
 
 
-def artifact_best_unit_styles():
-    response = [[]]
-    return
+def artifact_best_unit_styles(filtered_artifacts, req_sub_stats, size):
+    response = [
+        [
+            [{"artifact": {}, "value": 0} for m in range(len(req_sub_stats))]
+            for k in range(size)
+        ]
+        for i, _ in enumerate(Artifact.UNIT_STYLES_STRINGS.items())
+    ]
+    # List to not duplicate artifacts in the response
+    artifacts_list = []
+    # Get the best sub_stats of artifacts of each attribute
+    for i, unit_style in enumerate(Artifact.UNIT_STYLES_STRINGS.items()):
+        artifacts_list.clear()
+        for k in range(size):
+            for m, req_substat in enumerate(req_sub_stats):
+                curr_best_value = response[i][k][m]["value"]
+                curr_best_artifact = {}
+                for artifact in filtered_artifacts:
+                    if artifact.unit_style != unit_style[0]:
+                        continue
+                    # Get {size} bests artifact for each {req_sub_stats} in each of the {Artifact.ATTRIBUTE_STRINGS.items()}
+                    for o, artif_substat in enumerate(artifact.sec_effects):
+                        if artif_substat[0] == req_substat:
+                            if (
+                                artif_substat[1] >= curr_best_value
+                                and artifact not in artifacts_list
+                            ):
+                                response[i][k][m]["artifact"] = artifact.toJSON()
+                                if curr_best_artifact in artifacts_list:
+                                    artifacts_list.remove(curr_best_artifact)
+                                curr_best_value = artif_substat[1]
+                                curr_best_artifact = artifact
+                                artifacts_list.append(artifact)
+                                break
+                response[i][k][m]["value"] = curr_best_value
+    return jsonify(response)
 
 
 # Sort the artifacts by the substat received in the request, and return the best ones
@@ -85,26 +118,3 @@ def get_bests_artifacts():
     if type == 1:
         return artifact_best_attributes(filtered_artifacts, req_sub_stats, size)
     return artifact_best_unit_styles(filtered_artifacts, req_sub_stats, size)
-
-    # for i, artifact in enumerate(filtered_artifacts):
-    #     if artifact.attribute == 1:
-    #         if len(fire) < size:
-    #             fire.append(artifact)
-    #         else:
-    #             for artifact in fire:
-    #                 print(artifact)
-    #             for curr_best_artif in reversed(fire):
-    #                 for k, substat_effect in enumerate(curr_best_artif.sec_effects):
-    #                     if substat in Artifact.EFFECT_STRINGS.get(
-    #                         substat_effect[0], ""
-    #                     ):
-    #                         if (
-    #                             substat_effect[1]
-    #                             > artifact.sec_effects[subs_indexes[i]][1]
-    #                         ):
-    #                             fire.remove(curr_best_artif)
-    #                             fire.append(artifact)
-    #                             break
-    #             for artifact in fire:
-    #                 print(artifact)
-    #             break
